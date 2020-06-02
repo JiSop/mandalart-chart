49243e0940f703654bdabf146c58e0e78215fbb6
JiSop <jisop.shin@gmail.com>
2020-06-02 16:28:25 +0900

    초기화와 환경설정

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+import Mandalart from "./component/Mandalart";
+
+const App = () => {
+  return (
+    <div id="App">
+      <Mandalart />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Mandalart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mandalart = () => {
+  return <div className="square"></div>;
+};
+
+export default Mandalart;
