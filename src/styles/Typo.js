5e7977003ad1d1392d4845040ceb0b2e0c581cc1
JiSop <jisop.shin@gmail.com>
2020-06-09 13:49:14 +0900

    Typo 스타일 셋 추가
    텍스트 스타일 셋

---
+++ src/styles/Typo.js
@@ -0,0 +1,21 @@
+import { css } from 'styled-components';
+
+export default {
+  subGoalPlan: css`
+    font-family: Noto Sans KR;
+    font-size: 11.5px;
+    line-height: 16px;
+  `,
+  subGoal: css`
+    font-family: Noto Sans KR;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 20px;
+  `,
+  goal: css`
+    font-family: Noto Sans KR;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 24px;
+  `,
+};
