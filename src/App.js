59e182c6b8afeb13fd530d268e835f33cad4ca24
JiSop <jisop.shin@gmail.com>
2020-09-12 12:17:43 +0900

    Add Header, Footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import 'normalize.css';
 import MandalartContainer from './containers/MandalartContainer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div id="App">
+      {/* <Header /> */}
       <MandalartContainer />
+      {/* <Footer /> */}
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = props => {
+  return <h1>만다라차트</h1>;
+};
+
+Header.propTypes = {};
+
+export default Header;
